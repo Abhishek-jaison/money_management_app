9a843b40a7ab580d10983bbd08c8d3d7679e5d5c
Abhishek <abhishekjaison04@gmail.com>
2024-05-14 00:14:00 +0530

    new UI

--- lib/screens/categories/screen_category.dart
+++ lib/screens/categories/screen_category.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:money_management_app/screens/categories/expense_category_listview.dart';
+import 'package:money_management_app/screens/categories/income_category_listview.dart';
 
-class ScreenCategory extends StatelessWidget {
+class ScreenCategory extends StatefulWidget {
   const ScreenCategory({super.key});
 
   @override
+  State<ScreenCategory> createState() => _ScreenCategoryState();
+}
+
+
+class _ScreenCategoryState extends State<ScreenCategory> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _tabController=TabController(length: 2,vsync: this);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Category'));
+    return Column(
+      children: [
+        TabBar(
+          controller: _tabController,
+          tabs: [
+        Tab(text: 'INCOME',),
+        Tab(text: 'EXPENSE',),
+      ]),
+      Expanded(
+        child: TabBarView(
+          controller: _tabController,
+          children: const [
+            IncomeCategoryListView(),
+            ExpenseCategoryListView()
+        ],),
+      )
+        
+      ],
+    );
   }
 }
